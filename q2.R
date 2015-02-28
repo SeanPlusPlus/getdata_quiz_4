de27ed652b3d0f80bb07505b499fb694cfd6a1e9
Sean Stephenson <sms@pixar.com>
2015-02-28 10:32:31 -0800

    scaffold out q2

---
+++ q2.R
@@ -0,0 +1,35 @@
+# Load the Gross Domestic Product data for the 190 ranked countries in this 
+# data set: 
+#
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv 
+#
+# Remove the commas from the GDP numbers in millions of dollars and average them. 
+# What is the average? 
+#
+# Original data sources: http://data.worldbank.org/data-catalog/GDP-ranking-table
+
+################################################################################
+getData <- function() {
+################################################################################
+
+  # add data dir
+  if (!file.exists("data")) {
+    dir.create("data")
+  }
+
+  # fetch and return
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+  download.file(fileUrl, destfile = "./data/data2.csv", method = "curl")
+  data <- read.csv("./data/data2.csv")
+  return(data)
+}
+
+################################################################################
+main <- function() {
+################################################################################
+  data <- getData()
+}
+
+#main()
+
+# ""   "15"
