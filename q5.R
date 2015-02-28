1d712b762bcdd698b22ccb570fb75fb7182fa16d
Sean Stephenson <sms@pixar.com>
2015-02-28 11:18:47 -0800

    scaffold out q5

---
+++ q5.R
@@ -0,0 +1,27 @@
+#You can use the quantmod (http://www.quantmod.com/) package to get historical stock prices for
+#publicly traded companies on the NASDAQ and NYSE. Use the following code to download data on
+#Amazon's stock price and get the times the data was sampled.
+#
+#library(quantmod)
+#amzn = getSymbols("AMZN",auto.assign=FALSE)
+#sampleTimes = index(amzn) 
+#
+#How many values were collected in 2012? How many values were collected on Mondays in 2012?
+
+################################################################################
+getData <- function() {
+################################################################################
+  install.packages('quantmod')
+  library(quantmod)
+  amzn = getSymbols("AMZN",auto.assign=FALSE)
+  sampleTimes = index(amzn) 
+  return(sampleTimes)
+}
+
+################################################################################
+main <- function() {
+################################################################################
+  data  <- getData()
+}
+
+# main()
